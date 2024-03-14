4caeed06e4bec834530a517b9b6cf6f4f5fe80e7
Carlos Peláez González <carlosprog1997@gmail.com>
2024-03-14 16:01:31 +0100

    Fixed indentation for other modes like fundamental-mode

--- config/general/editor.el
+++ config/general/editor.el
@@ -1,4 +1,19 @@
 
 ; Disable adding a tab when creating a new line in text mode
-(add-hook 'text-mode-hook
-            (lambda () (electric-indent-local-mode -1)))
+; https://emacs.stackexchange.com/questions/76938/how-do-i-turn-off-electric-indent-mode-within-fundamental-mode
+(with-eval-after-load 'electric
+  (electric-indent-mode -1))
+
+(defcustom my-electric-indent-exclusion-list '(text-mode fundamental-mode)
+  "Major modes where `electric-indent-local-mode' is not switched on."
+  :group 'my
+  :type '(repeat symbol))
+
+(defun my-switch-on-electric-indent ()
+  "Maybe switch on `electric-indent-local-mode'.
+Do not activate electric indent if the major mode is
+in `my-electric-indent-exclusion-list'."
+  (unless (apply #'derived-mode-p my-electric-indent-exclusion-list)
+    (electric-indent-local-mode)))
+
+(add-hook 'after-change-major-mode-hook #'my-switch-on-electric-indent)
